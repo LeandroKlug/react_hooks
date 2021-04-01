681cfda6493a14ad71b7f67d68db338c6f30b108
LeandroKlug <leandro.softnetwork@gmail.com>
2021-04-01 13:34:07 -0300

    iniciando com useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  //componentDidUpdate - executa sempre
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+  //componentDidMount - executa uma vez
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, []);
+
+  //componentDidMount([com dependencia]) - executa toda vez que a dependencia mudar
+  useEffect(() => {
+    console.log('Contador mudou para', counter);
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>Contador: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Adicionar</button>
+      <button onClick={() => setCounter(counter - 1)}>Diminuir</button>
     </div>
   );
 }
