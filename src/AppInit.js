9c51f81f42ceb73ce3c406a3624212870f341f86
LeandroKlug <leandro.softnetwork@gmail.com>
2021-04-05 10:06:22 -0300

    utilizando useContext

---
+++ src/AppInit.js
@@ -0,0 +1,12 @@
+import P from 'prop-types';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Oi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/useContext.js
@@ -0,0 +1,63 @@
+import React, { useContext, useState } from 'react';
+import './App.css';
+
+const globalState = {
+  title: 'O titulo do contexto',
+  body: 'o corpo da mensagem',
+  counter: 0,
+};
+
+const GlobalContext = React.createContext();
+
+// eslint-disable-next-line
+const Div = ({ children }) => {
+  return (
+    <>
+      <H1 />
+      <P />
+    </>
+  );
+};
+
+// eslint-disable-next-line
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { title, counter },
+  } = theContext;
+  console.log(theContext);
+  return (
+    <h1>
+      {title}
+      {counter}
+    </h1>
+  );
+};
+
+// eslint-disable-next-line
+const P = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { body, counter },
+    setContextState,
+  } = theContext;
+  return (
+    <p
+      onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}
+    >
+      {body}
+    </p>
+  );
+};
+
+function App() {
+  const [contextState, setContextState] = useState(globalState);
+
+  return (
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
+  );
+}
+
+export default App;
